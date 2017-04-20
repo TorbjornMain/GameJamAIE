cacd055b53018dae5207a08168b1ec7ebfd71845
Zachary De Maria <s171558@ad.aie.edu>
2017-04-20 10:05:49 +0930

    Cameras working and designer friendly

--- Assets/Scripts/Detection/CameraDetection.cs
+++ Assets/Scripts/Detection/CameraDetection.cs
@@ -3,25 +3,82 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-[RequireComponent(typeof(Rigidbody2D))]
 public class CameraDetection : MonoBehaviour {
-    
-    Rigidbody2D rb;
     public bool detected;
+    bool prevDetected = false;
+
+
+    [SerializeField()]
+    [Range(0, 180)]
+    float FOV;
+    [SerializeField()]
+    [Range(0,10)]
+    float radius;
+    [SerializeField()]
+    List<GameObject> linkedObjects;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+
     }
 
-    void OnTriggerEnter2D(Collider2D other)
+    void Update()
     {
-        detected = true;
+        GameObject target = GameObject.FindGameObjectWithTag("Player");
+        if (target)
+        {
+
+            float ang = Vector2.Angle(transform.right, (target.transform.position - transform.position).normalized);
+            print(ang);
+            if (ang < FOV / 2)
+            {
+                
+                RaycastHit2D wallCheck = Physics2D.Raycast(transform.position, target.transform.position - transform.position, radius);
+                if (wallCheck)
+                {
+                    detected = wallCheck.collider.gameObject == target;
+                }
+                else
+                {
+                    detected = false;
+                }
+
+            }
+            else
+            {
+                detected = false;
+            }
+        }
+        else
+        {
+            detected = false;
+        }
+
+        if((detected != prevDetected) && detected)
+        {
+            foreach (GameObject g in linkedObjects)
+            {
+                SendMessage("Activate");
+            }
+        }
+        prevDetected = detected;
     }
 
-    private void OnTriggerExit2D(Collider2D other)
+    void OnDrawGizmos()
     {
-        detected = false;
+        if (detected)
+        {
+            Gizmos.color = Color.green;
+        }
+        else
+        {
+            Gizmos.color = Color.red;
+        }
+
+        Gizmos.DrawLine(transform.position, transform.position + Quaternion.Euler(0, 0, -FOV / 2) * new Vector3(radius, 0, 0));
+        Gizmos.DrawLine(transform.position, transform.position + Quaternion.Euler(0, 0, FOV / 2) * new Vector3(radius, 0, 0));
+        Gizmos.DrawLine(transform.position + Quaternion.Euler(0, 0, -FOV / 2) * new Vector3(radius, 0, 0), transform.position + transform.rotation * new Vector3(radius, 0, 0));
+        Gizmos.DrawLine(transform.position + Quaternion.Euler(0, 0, FOV / 2) * new Vector3(radius, 0, 0), transform.position + transform.rotation * new Vector3(radius, 0, 0));
     }
 
 }
