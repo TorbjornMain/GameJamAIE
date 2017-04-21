aa0d3d9c53405b4369b9b3748f7e709413159f49
AIE\s171435 <Zeldafan520@hotmail.com>
2017-04-21 13:46:19 +0930

    Manual Merge Patch (sometimes two wrongs make a right)

--- Assets/Scripts/Detection/CameraDetection.cs
+++ Assets/Scripts/Detection/CameraDetection.cs
@@ -61,9 +61,6 @@ public class CameraDetection : MonoBehaviour {
         spotlight.color = clearColor;
         spotlight.range = radius - 1;
         spotlight.spotAngle = FOV;
-        Camera[] cam = { GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>() };
-        spotlight.GetComponent<LightShafts>().m_Cameras = cam;
-        
     }
 
     void Update()
@@ -100,7 +97,7 @@ public class CameraDetection : MonoBehaviour {
         if(!delayedDetectSent && detected)
         {
             timeDetected = Mathf.Min(Time.deltaTime + timeDetected, detectDelay);
-            if (timeDetected == detectDelay)
+            if (timeDetected > detectDelay)
             {
                 delayedDetectSent = true;
                 foreach (GameObject g in linkedObjects)
